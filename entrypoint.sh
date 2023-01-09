94696f531d3ea880122e2d03787930cae6f17932
QingqinKong <qingqin.kong@tonglec.org>
2023-01-09 15:47:06 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "Creating archive";
 # mkdir /out
-# cd $GITHUB_WORKSPACE
+cd $GITHUB_WORKSPACE
 ls
 # zip -r /out/repoarchive.zip ./* -x .git/*
 export FILE_PATH='/home/runner/work/SReading/SReading/android/app/build/outputs/apk/release/app-release-signed.apk'
