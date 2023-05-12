2b2a2aec33672df9964f92e1ef091592adfc4b01
kqq <kqq@papaz.me>
2023-05-12 20:31:36 +0800

    debug

--- index.js
+++ index.js
@@ -1,48 +1,54 @@
 var spsave = require("spsave").spsave;
-var fs = require('fs');
+var fs = require("fs");
 
 function trimSlashes(string) {
-    return string.replace(new RegExp('/', 'g'), '_');
+  return string.replace(new RegExp("/", "g"), "_");
 }
 
 var coreOptions = {
-    siteUrl: process.env.SITE_URL,
+  siteUrl: process.env.SITE_URL,
 };
 var creds = {
-    username: process.env.USER,
-    password: process.env.PASSWD
+  username: process.env.USER,
+  password: process.env.PASSWD,
 };
 
-var now = new Date().toISOString().replace(/[-.:TZ]/g, '').slice(2, 12);
+var now = new Date()
+  .toISOString()
+  .replace(/[-.:TZ]/g, "")
+  .slice(2, 12);
 
 var ref = "";
 if (process.env.GITHUB_REF) {
-    ref = process.env.GITHUB_REF.substr(process.env.GITHUB_REF.lastIndexOf('/') + 1);
+  ref = process.env.GITHUB_REF.substr(
+    process.env.GITHUB_REF.lastIndexOf("/") + 1
+  );
 }
 var fileOptions = {};
-if (process.env.APK_NAME && process.env.APK_URL) {
-    fileOptions = {
-        folder: process.env.LIB_FOLDER,
-        fileName: `${trimSlashes(process.env.APK_NAME)}_${now}.apk`,
-        fileContent: fs.readFileSync(process.env.APK_URL)
-    };
-} else if (process.env.GLOB_URL) {
-    if (process.env.GLOB_URL == "") {
-        return;
-    }
-    fileOptions = {
-        folder: process.env.LIB_FOLDER,
-        glob: process.env.GLOB_URL,
-    };
+if (process.env.APK_NAME != "" && process.env.APK_URL != "") {
+  console.log("upload file");
+  fileOptions = {
+    folder: process.env.LIB_FOLDER,
+    fileName: `${trimSlashes(process.env.APK_NAME)}_${now}.apk`,
+    fileContent: fs.readFileSync(process.env.APK_URL),
+  };
+} else if (process.env.GLOB_URL != "") {
+  console.log("upload glob");
+  fileOptions = {
+    folder: process.env.LIB_FOLDER,
+    glob: process.env.GLOB_URL,
+  };
 }
 
-if (Object.keys(fileOptions).length == 0)
-    return;
+if (Object.keys(fileOptions).length == 0) {
+  console.log("nothing to upload");
+  return;
+}
 
 spsave(coreOptions, creds, fileOptions)
-    .then(function () {
-        console.log('Success');
-    })
-    .catch(function (err) {
-        process.exit(1);
-    });
+  .then(function () {
+    console.log("Success");
+  })
+  .catch(function (err) {
+    process.exit(1);
+  });
